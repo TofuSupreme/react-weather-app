e1334e606b8d0960a3eb24f4ebb54156ead59748
tofusupreme <yunishbacon@gmail.com>
2022-08-19 21:18:30 +0900

    updated api component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import { AsyncPaginate } from "react-select-async-paginate"
+import { GEO_API_URL, geoApiOptions} from './api';
 
 const Search = ({onSearchChange}) => {
 
   const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    return fetch(`${GEO_API_URL}cities?minPopulation=10000&namePrefix=${inputValue}`, geoApiOptions)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+  }
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
@@ -16,6 +24,7 @@ const Search = ({onSearchChange}) => {
       debounceTimeout={500}
       value={search}
       onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
   )
 }
