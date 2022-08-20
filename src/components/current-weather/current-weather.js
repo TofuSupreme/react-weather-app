9c6062e00f7a499eff7fc24a7da8e49b4432e5ac
tofusupreme <yunishbacon@gmail.com>
2022-08-20 15:45:57 +0900

    Added hardcoded text for current-weather parameters

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -8,9 +8,33 @@ const CurrentWeather = () => {
           <p className="city">Kashima</p>
           <p className="weather-description">Cloudy</p>
         </div>
-        <img alt="weather" className="weather-icon" src="icons/01d.png"/>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
-    </div>
+      <div className="bottom">
+        <p className="temperature">22°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label"> Details </span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label"> Feels like </span>
+              <span className="parameter-value"> 18°C</span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label">Wind</span>
+              <span className="parameter-value"> 2 m/s</span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label"> Humidity </span>
+              <span className="parameter-value"> 15%</span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label">Pressure</span>
+              <span className="parameter-value"> 15 hPa</span>
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
