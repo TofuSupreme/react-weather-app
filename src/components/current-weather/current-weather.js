506770750756b191c1e42749f427da34ba06e6e6
tofusupreme <yunishbacon@gmail.com>
2022-08-20 15:29:20 +0900

    added styling to weather class

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -4,10 +4,12 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city">Kashima</p>
-        <p className="weather-description">Cloudy</p>
+        <div>
+          <p className="city">Kashima</p>
+          <p className="weather-description">Cloudy</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png"/>
       </div>
-      <img alt="weather" className="weather-icon" src=""/>
     </div>
   )
 }
