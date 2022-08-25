0d170475757e14c953dcaf032895bd6db72cab81
tofusupreme <yunishbacon@gmail.com>
2022-08-25 21:27:51 +0900

    Revert "edited styling to index.css and forecast.css"
    This reverts commit 5684a13d4317c16b928c184e02d61086c3881535.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
-import "./App.css";
+import './App.css';
 import Search from "./components/search/search";
-import CurrentWeather from "./components/current-weather/current-weather";
-import Forecast from "./components/forecast/forecast";
-import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
-import { useState } from "react";
+import CurrentWeather from './components/current-weather/current-weather';
+import Forecast from './components/forecast/forecast';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
+import { useState } from 'react';
 
 function App() {
-	const [currentWeather, setCurrentWeather] = useState(null);
-	const [currentForecast, setForecast] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [currentForecast, setForecast] = useState(null);
 
-	const handleOnSearchChange = (searchData) => {
-		const [lat, lon] = searchData.value.split(" ");
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split("");
 
-		const currentWeatherFetch = fetch(
-			`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-		);
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
 
-		const forecastFetch = fetch(
-			`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-		);
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
 
-		Promise.all([currentWeatherFetch, forecastFetch])
-			.then(async (response) => {
-				const weatherResponse = await response[0].json();
-				const forecastResponse = await response[1].json();
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+      const weatherResponse  = await response[0].json();
+      const forecastResponse = await response[1].json()
 
-				setCurrentWeather({ city: searchData.label, ...weatherResponse });
-				setForecast({ city: searchData.label, ...forecastResponse });
-			})
-			.catch((err) => console.log(err));
-	};
-	console.log(currentWeather);
-	console.log(currentForecast);
+        setCurrentWeather({city: searchData.label, ...weatherResponse});
+        setForecast({city: searchData.label, ...forecastResponse});
+    })
+      .catch((err) => console.log(err));
+  }
+  console.log(currentWeather);
+  console.log(currentForecast);
 
-	return (
-		<div className="container">
-			<Search onSearchChange={handleOnSearchChange} />
-			{currentWeather && <CurrentWeather data={currentWeather} />}
-			{currentForecast && <Forecast data={currentForecast} />}
-		</div>
-	);
+  return (
+
+<div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {currentForecast && <Forecast data={currentForecast} />}
+    </div>
+  );
 }
 
 export default App;
